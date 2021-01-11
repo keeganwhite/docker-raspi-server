4c47b9fc0da4506f90f4e95e1445693b794f6120
Keegan White <keeganthomaswhite@gmail.com>
2021-01-11 10:58:50 +0200

    Fixed build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -3,6 +3,7 @@
 docker network create --attachable -d macvlan  --subnet=10.2.0.0/16  --gateway=10.2.0.1  -o parent=eth0 inethi-bridge
 cd ./performance-monitoring
 ./local_build.sh
+cd ..
 cd ./dns-masq
 ./local_build.sh
 echo done
