94833859b16ce2116a286ca3c5a286ddc134d1d0
Keegan White <keeganthomaswhite@gmail.com>
2021-11-07 16:48:27 +0200

    Starting redesign

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker network create --attachable -d macvlan  --subnet=10.2.0.0/16  --gateway=10.2.0.1  -o parent=eth0 inethi-bridge
-systemctl disable --now systemd-resolved # disable port 53 from being used by ubuntu
-cd ./performance-monitoring
-./local_build.sh
-cd ..
-
-echo done
