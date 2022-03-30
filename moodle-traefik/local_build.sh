a2b7ce0a884b48dd203cb818092f733d19d0ca65
Keegan White <keeganthomaswhite@gmail.com>
2022-03-30 15:03:06 +0200

    Updated git ignore

--- moodle-traefik/local_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /mnt/data/moodle
-sudo chmod -R 775 /mnt/data/moodle
-docker-compose up -d
--- traefik-with-dnsmasq/local_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo mkdir /mnt/data/dnsmasq
-sudo mkdir /mnt/data/traefik
-sudo cp -r ./dnsmasq/* /mnt/data/dnsmasq
-
-docker-compose up -d
